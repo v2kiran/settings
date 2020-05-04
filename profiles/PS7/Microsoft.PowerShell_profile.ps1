33ab918eabca8805ee7de47c0035c6f54e3b01e2
Kiran Reddy <v.2kirar@gmail.com>
2020-05-04 11:07:47 +0530

    updated profiles

--- profiles/PS7/Microsoft.PowerShell_profile.ps1
+++ profiles/PS7/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,15 @@
-Set-Location c:\gh -ErrorAction Stop
+Set-Location C:\ghe\
+
+Import-Module PSColor
 Import-Module oh-my-posh
 Set-Theme Paradox
+
+$dc = ($env:LOGONSERVER -replace '\\\\')
+
+#psreadline
+. "C:\gh\settings\psreadline\Sample.profile.ps1"
+
+Function Elevate
+{
+  Start-Process (Get-Process -id $PID).path -verb runas
+}
--- profiles/Win PS 5.1/Microsoft.PowerShell_profile.ps1
+++ profiles/Win PS 5.1/Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,39 @@
 # this imports the bookmarks
 #import-folderbookmark
 
+Import-Module PSColor
+<#
+Import-Module "C:\ghe\Cisco.DS.Password\src\Cisco.DS.Password.psd1" -Force
+Import-Module "C:\ghe\Cisco.DS.YubiKey\src\Cisco.DS.YubiKey.psd1" -Force
+Import-Module "C:\ghe\Cisco.DS.Certificate\src\Cisco.DS.Certificate.psd1" -Force
+import-folderbookmark
+#>
+
+
+#psreadline
+
 Import-Module oh-my-posh
 Set-Theme Paradox
 
 # Goto location
 Set-Location c:\gh
+
+
+# Goto location
+$dc = ($env:LOGONSERVER -replace '\\\\')
+
+
+. "C:\gh\settings\psreadline\Sample.profile.ps1"
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile))
+{
+  Import-Module "$ChocolateyProfile"
+}
+
+
+Function Elevate
+{
+  Start-Process (Get-Process -id $PID).path -verb runas
+}
