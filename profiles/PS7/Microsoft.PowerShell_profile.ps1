8e9d5de295802dcc91584101573d888d444e14ef
Kiran Reddy <v.2kirar@gmail.com>
2022-03-20 13:47:49 +0530

    updated profiles

--- profiles/PS7/Microsoft.PowerShell_profile.ps1
+++ profiles/PS7/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,10 @@
 Set-Location C:\gh\
-#$PSDefaultParameterValues.Add('Out-Default:OutVariable', 'Lastout')
+
+#Import-Module Poshcolor
+Import-Module -Name Terminal-Icons
+Import-Module oh-my-posh
+#Import-Module c:\gh\misc -Force -DisableNameChecking
+#Set-PoshPrompt -Theme 'C:\gh\settings\themes\my-paradox.json'
 
 # 3. Set Format enumeration olimit
 $FormatEnumerationLimit = 99
@@ -12,12 +17,15 @@ $PSDefaultParameterValues = @{
     'Out-Default:OutVariable' = 'Lastout'
 }
 
-#Import-Module Poshcolor
-Import-Module -Name Terminal-Icons
-Import-Module oh-my-posh
-Import-Module c:\gh\misc -Force -DisableNameChecking
-Set-PoshPrompt -Theme 'C:\gh\settings\themes\my-paradox.json'
-New-PSDrive -Name Z -PSProvider FileSystem -Root \\archie-ds\winroot -Persist
+
+$omp_config = 'c:\gh\settings\config\takuya.omp.json'
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+if (Test-Path \\archie-ds\winroot)
+{
+    New-PSDrive -Name Z -PSProvider FileSystem -Root \\archie-ds\winroot -Persist -ErrorAction SilentlyContinue
+}
+
 
 $PSStyle.Formatting.TableHeader = $PSStyle.Foreground.BrightYellow
 $PSStyle.Formatting.FormatAccent = $PSStyle.Foreground.BrightYellow
@@ -30,3 +38,9 @@ Function Elevate
 {
     Start-Process (Get-Process -Id $PID).path -Verb runas
 }
+Import-Module Az.Tools.Predictor
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
--- profiles/Win PS 5.1/Microsoft.PowerShell_profile.ps1
+++ profiles/Win PS 5.1/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,26 @@
 #psdefault values
-$PSDefaultParameterValues.Add('Out-Default:OutVariable', 'Lastout')
+# 3. Set Format enumeration olimit
+$FormatEnumerationLimit = 99
+
+# 4. Set some command defaults
+$PSDefaultParameterValues = @{
+    '*:autosize'              = $true
+    'Receive-Job:keep'        = $true
+    '*:Wrap'                  = $true
+    'Out-Default:OutVariable' = 'Lastout'
+}
 # this imports the bookmarks
 #import-folderbookmark
 
 #Import-Module Poshcolor
 Import-Module -Name Terminal-Icons
-Import-Module c:\gh\misc -Force
+#Import-Module c:\gh\misc -Force
 #psreadline
 
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme 'C:\Users\Kiran\OneDrive\GitHub\settings\themes\my-paradox.json'
+$omp_config = 'c:\gh\settings\config\takuya.omp.json'
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+#Set-PoshPrompt -Theme 'C:\Users\Kiran\OneDrive\GitHub\settings\themes\my-paradox.json'
 
 # Goto location
 Set-Location c:\gh
@@ -24,8 +35,11 @@ if (Test-Path($ChocolateyProfile))
     Import-Module "$ChocolateyProfile"
 }
 
-
+Set-PSReadLineOption -PredictionViewStyle ListView
 Function Elevate
 {
     Start-Process (Get-Process -Id $PID).path -Verb runas
 }
+
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
